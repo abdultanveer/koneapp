ad1b9c5c9a09dce900d92f1b0c42a105a2715335
abdul <abdul.tanveer@gmail.com>
2026-03-30 17:33:00 +0530

    image composable

--- app/src/main/java/tanveer/ansari/koneapp/MainActivity.kt
+++ app/src/main/java/tanveer/ansari/koneapp/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -13,6 +16,9 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import tanveer.ansari.koneapp.ui.theme.KoneappTheme
@@ -27,8 +33,10 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    myCommposable()
-                }
+                    GreetingImage(
+                        message = stringResource(R.string.wishes),
+                        from = stringResource(R.string.wisher)
+                    )                }
             }
         }
     }
@@ -47,7 +55,8 @@ fun myCommposable(){
 fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
     Column {
         Text(
-            text = message,
+            //modifier = Modifier.background(color = Color.Green),
+                    text = message,
             fontSize = 100.sp,
             lineHeight = 116.sp,
         )
@@ -58,11 +67,29 @@ fun GreetingText(message: String, from: String, modifier: Modifier = Modifier){
     }
 }
 
+@Composable
+fun GreetingImage(message: String, from: String) {
+    Box {
+        val image = painterResource(R.drawable.androidpart)
+        Image(
+            alpha = 0.5F,
+                    painter = image,
+            contentDescription = null
+        )
+        GreetingText(message, from)
+    }
+
+}
+
+
+
 @Preview(showBackground = true,
     )
 @Composable
 fun GreetingPreview() {
     KoneappTheme {
-        GreetingText(message = "Happy Birthday Sam!", from = "abdul")
-    }
+        GreetingImage(
+            message = "Happy Birthday Sam!",
+            from = "From Emma"
+        )    }
 }
